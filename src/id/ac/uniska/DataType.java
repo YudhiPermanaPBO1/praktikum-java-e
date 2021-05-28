7d8863286a75d78b3e043265a6a20dac6f125f1d
YudhiPermanaPBO1 <yudhi.gotan.10@gmail.com>
2021-05-28 18:42:06 +0800

    Belajar Type Data Pada Java

--- src/id/ac/uniska/DataType.java
+++ src/id/ac/uniska/DataType.java
@@ -1,4 +1,28 @@
 package id.ac.uniska;
 
+import javax.swing.*;
+
 public class DataType {
+
+    public static void main(String[] args) {
+        String namaDepan    = "Yudhi";
+        String namaTengah   = "Permana";
+        String namaBelakang = "";
+        int usia            = 19;
+        int targetTahunKuliah = 4;
+        double ipk            = 3.8;
+        char nilaiAbjad       = 'A';
+        boolean tampan        = true;
+
+        System.out.println("Nama Depan :" + namaDepan);
+        System.out.println("Nama Tengah :" + namaTengah);
+        System.out.println("Nama Belakang :" + namaBelakang);
+        System.out.println("Usia :" + usia);
+        System.out.println("Target Kuliah :" + targetTahunKuliah + " tahun");
+        System.out.println("IPK :" + ipk);
+        System.out.println("Nilai Abjad :" + nilaiAbjad);
+        System.out.println("Tampan " + tampan);
+
+        JOptionPane.showMessageDialog(null, "Halo "+namaDepan+" "+namaTengah+" "+namaBelakang, "Ini Judul", JOptionPane.WARNING_MESSAGE);
+    }
 }
